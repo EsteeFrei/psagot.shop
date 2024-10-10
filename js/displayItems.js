298967b23fb1f3cedf7d2f79b0326048cea6de6f
Estee <e533163610@gmail.com>
2024-10-10 11:44:08 +0300

    display-item- begining

--- js/displayItems.js
+++ js/displayItems.js
@@ -1,17 +1,40 @@
 
-import  notebooks  from '..products.json/' assert {type: "json"}
-import { craft } from './products'
+import  data  from '.././products.json' with {type: "json"};
 
+console.log(data.notebooks);
+console.log();
 
-console.log(notebooks);
 
 function displayItems() {
     let content = document.getElementById('content')
     let item;
-    for (let i = 0; i < notebooks.length; i++) {
+    for (let i = 0; i < data.notebooks.length; i++) {
         item = document.createElement('div')
-        item.innerText(notebooks[i].image)
+        item.classList.add('item')
+        let img = document.createElement('img');
+        img.src = data.notebooks[i].image; 
+        img.alt = data.notebooks[i].title; 
+        img.classList.add('img')
+        let txt = document.createElement('h3'); 
+        txt.innerText = data.notebooks[i].title; 
+        img.classList.add('img')
+        item.appendChild(txt)
+        item.appendChild(img)
         content.appendChild(item)
-
+        console.log(i);
+        
     }
+    // for (let j = 0; j < data.craft.length; j++) {
+    //     item = document.createElement('div')
+    //     item.classList.add('item')
+    //     let img = document.createElement('img');
+    //                 img.src = data.craft[j].image; 
+    //                 img.alt = data.craft[j].title; 
+    //     item.appendChild(img)
+    //     content.appendChild(item)
+    //     console.log(j);
+        
+    // }
 }
+
+displayItems()
