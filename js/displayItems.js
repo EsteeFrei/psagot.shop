2b66e8171ad15b67e0bd058595a48d37a1ebd81a
Estee <e533163610@gmail.com>
2024-10-13 09:55:53 +0300

    coplete content

--- js/displayItems.js
+++ js/displayItems.js
@@ -1,40 +1,69 @@
 
-import  data  from '.././products.json' with {type: "json"};
-console.log("gyjfcjxzytshxy");
-
-console.log(data.notebooks);
+import data from '.././products.json' with {type: "json"};
 
 
-function displayItems() {
+function displayItems(items) {
     let content = document.getElementById('content')
     let item;
-    for (let i = 0; i < data.notebooks.length; i++) {
+
+    let title = document.getElementById('title')
+    title.classList.add('title')
+    
+    let titleDelivery= document.createElement('p')
+    titleDelivery.classList.add('titleDelivery')
+    titleDelivery.textContent="משלוחים חינם בפסגות אפק"
+
+    let titleType= document.createElement('h1')
+    titleType.classList.add('titleType')
+    titleType.textContent="כלי כתיבה"
+
+
+    let titleImg = document.createElement('img')
+    titleImg.classList.add('titleImg')
+    titleImg.src = "../imgs/pic1.png"
+
+    title.appendChild(titleDelivery)
+    title.appendChild(titleType)
+    title.appendChild(titleImg)
+   
+    for (let i = 0; i < items.length; i++) {
         item = document.createElement('div')
         item.classList.add('item')
+
+        let imgDiv = document.createElement('div')
+        imgDiv.classList.add('imgDiv')
         let img = document.createElement('img');
-        img.src = data.notebooks[i].image; 
-        img.alt = data.notebooks[i].title; 
+        img.src = items[i].image;
+        img.alt = items[i].title;
         img.classList.add('img')
-        let txt = document.createElement('h3'); 
-        txt.innerText = data.notebooks[i].title; 
-        img.classList.add('img')
-        item.appendChild(txt)
-        item.appendChild(img)
+        imgDiv.appendChild(img)
+
+        let txtDiv = document.createElement('div');
+        txtDiv.classList.add('txtDiv')
+        let txt = document.createElement('p');
+        txt.innerText = items[i].title;
+        txtDiv.appendChild(txt)
+
+        let buyDiv = document.createElement('div');
+        buyDiv.classList.add('buyDiv')
+
+        let price = document.createElement('p')
+        price.innerText = "₪ " + " " + items[i].price;
+        price.classList.add('price')
+
+        let cart = document.createElement('button')
+        cart.classList.add('cartBtn')
+        cart.innerText = "הוספה לסל"
+
+        buyDiv.appendChild(cart)
+        buyDiv.appendChild(price)
+
+
+        item.appendChild(txtDiv)
+        item.appendChild(imgDiv)
+        item.appendChild(buyDiv)
         content.appendChild(item)
-        console.log(i);
-        
     }
-    // for (let j = 0; j < data.craft.length; j++) {
-    //     item = document.createElement('div')
-    //     item.classList.add('item')
-    //     let img = document.createElement('img');
-    //                 img.src = data.craft[j].image; 
-    //                 img.alt = data.craft[j].title; 
-    //     item.appendChild(img)
-    //     content.appendChild(item)
-    //     console.log(j);
-        
-    // }
 }
 
-displayItems()
+displayItems(data.craft)
